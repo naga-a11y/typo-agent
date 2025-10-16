53cf8107771ec5e8482c09dddb9a6ed175985842
naga-a11y <naga@typoapp.io>
2025-10-16 11:00:19 +0530

    Update faq_agent.py

--- faq_agent.py
+++ faq_agent.py
@@ -1,13 +1,13 @@
 # faq_agent.py
 from google.adk.agents.llm_agent import LlmAgent
 from toolbox_core import ToolboxSyncClient
-from constants import FAQ_PROMPT, TOOLBOX_URL
+from constants import FAQ_PROMPT, TOOLBOX_URL, DEFAULT_VERTEX_AI_MODEL_NAME
 
 toolbox = ToolboxSyncClient(TOOLBOX_URL)
 
 faq_agent = LlmAgent(
     name="FAQAgent",
-    model="gemini-2.0-flash",
+    model=DEFAULT_VERTEX_AI_MODEL_NAME,
     instruction=FAQ_PROMPT,
     tools=toolbox.load_toolset("cloudsql_faq_analysis_tools"),
 )
